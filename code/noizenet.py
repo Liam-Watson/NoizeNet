5f470636deba89f0a12aa11b2c97bc2243d5a66f
Liam Watson <you@example.com>
2021-10-05 19:30:24 +0200

    Added some trained models and support for LSTMs

--- code/noizenet.py
+++ code/noizenet.py
@@ -8,6 +8,12 @@ import librosa as lib
 import os
 import soundfile as sf #For writing
 import matplotlib.pyplot as plt
+import gc
+import sklearn.utils, sklearn.preprocessing, sklearn.decomposition, sklearn.svm
+import sklearn as skl
+import pandas as pd
+import utils
+
 
 # import wandb
 # wandb.init(project='noizenet', entity='liamwatson')
@@ -26,26 +32,18 @@ import matplotlib.pyplot as plt
 #     # 4. Log metrics to visualize performance
 #     wandb.log({"loss": loss})
 
-def normalize(v):
-    norm = np.linalg.norm(v)
-    if norm == 0:
-        return v
-    return v / norm
+#Get metadata for fma dataset
+# tracks = utils.load('data/fma_metadata/tracks.csv')
+# genres = utils.load('data/fma_metadata/genres.csv')
+# features = utils.load('data/fma_metadata/features.csv')
+# echonest = utils.load('data/fma_metadata/echonest.csv')
 
 files = os.listdir("/home/liam/Desktop/University/2021/MAM3040W/thesis/works/playground/wavs/fma_small/000/")
-# print(files)
-duration = 30
-y, sr = lib.load("/home/liam/Desktop/University/2021/MAM3040W/thesis/works/playground/wavs/fma_small/000/" + files[0], mono=True, duration = duration)
-# for x in range(y.size):
-#     if y[x] == 0:
-#         y[x] = 0
-#     elif abs(y[x]*10) < 1:
-#         y[x] = y[x]*10
-#     else:
-#         y[x] = 1
-# print(y, sr)
-# print(np.mean(y))
-sf.write('/home/liam/Desktop/University/2021/MAM3040W/thesis/writeup/code/uhh.wav', y, sr,format="wav")
+print(files)
+duration = 5
+y, sr = lib.load("/home/liam/Desktop/University/2021/MAM3040W/thesis/works/playground/wavs/fma_small/000/" + files[4], mono=True, duration = duration)
+sf.write('/home/liam/Desktop/University/2021/MAM3040W/thesis/writeup/code/input.wav', y, sr,format="wav")
+print(sr)
 
 
 seq_length = sr*duration
@@ -55,12 +53,12 @@ data = y
 data = np.resize(data,((seq_length+1), 1))
 x = data[:-1]
 y = data[1:]
-plt.plot(time_steps[1:], x, 'r.', label='input, x', markersize=0.1) # x
-plt.plot(time_steps[1:], y, 'b.', label='target, y', markersize=0.1) # y
+# plt.plot(time_steps[1:], x, 'r.', label='input, x', markersize=0.1) # x
+# plt.plot(time_steps[1:], y, 'b.', label='target, y', markersize=0.1) # y
 
-plt.legend(loc='best')
-plt.show(block = False)
-plt.pause(.001)
+# plt.legend(loc='best')
+# plt.show(block = False)
+# plt.pause(1)
 
 # First checking if GPU is available
 train_on_gpu=torch.cuda.is_available()
@@ -78,15 +76,25 @@ class NoizeNet(nn.Module):
 
             
         self.hidden_dim = hidden_dim
+        self.num_layers = n_layers
+        self.output_size = output_size
+        self.input_size = input_size
+
 
         # define an RNN with specified parameters
         # batch_first means that the first dim of the input and output will be the batch_size
-        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)
+        # self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)
+
+        #TODO: Test RNN vs LSTM
+        self.lstm = nn.LSTM(input_size, hidden_dim, n_layers, batch_first=True)
+        # self.hidden = (torch.zeros(1,1,self.hidden_dim), torch.zeros(1,1,self.hidden_dim)) #We need a tuple for a LSTM
+
 
         # last, fully-connected layer
         self.fc = nn.Linear(hidden_dim, output_size)
 
     def forward(self, x, hidden):
+    # def forward(self, x, hidden, c0): #LSTM!
         # x (batch_size, seq_length, input_size)
         # hidden (n_layers, batch_size, hidden_dim)
         # r_out (batch_size, time_step, hidden_size)
@@ -96,12 +104,19 @@ class NoizeNet(nn.Module):
         else:
             x.cpu()
         # get RNN outputs
-        r_out, hidden = self.rnn(x , hidden)
+        # r_out, hidden = self.rnn(x , hidden)
+        # r_out, (hidden, c0) = self.lstm(x , hidden) #LSTM!
+        #####LSTM
+        h_0 = torch.autograd.Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_dim)).cuda() #hidden state
+        c_0 = torch.autograd.Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_dim)).cuda() #internal state
+        # Propagate input through LSTM
+        r_out, (hn, cn) = self.lstm(x, (h_0, c_0)) #lstm with input, hidden, and internal state
+        hidden = hn
+        #####LSTM
+
         # shape output to be (batch_size*seq_length, hidden_dim)
         if (train_on_gpu):
             hidden.cuda()
-        else:
-            hidden
         r_out = r_out.view(-1, self.hidden_dim)
 
         # get final output
@@ -109,15 +124,14 @@ class NoizeNet(nn.Module):
 
         return output, hidden
 
-
 #####################################################################################################################
 
 # decide on hyperparameters
-n_steps = 10
+n_steps = 1
 input_size=1
 output_size=1
 hidden_dim=50
-n_layers=1
+n_layers=2
 
 # instantiate an RNN
 noizeNet = NoizeNet(input_size, output_size, hidden_dim, n_layers)
@@ -125,30 +139,39 @@ print(noizeNet)
 ######################################################################################################################
 
 # MSE loss and Adam optimizer with a learning rate of 0.01
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(noizeNet.parameters(), lr=0.01)
+# criterion = nn.MSELoss()
+criterion = nn.L1Loss()
+optimizer = torch.optim.Adam(noizeNet.parameters(), lr=0.001)
 
 ######################################################################################################################
 
 # train the RNN
-def train(noizeNet, n_steps, print_every, data, sr, files):
+def train(noizeNet, n_steps, print_every, data, sr, files, step_size=1):
     # initialize the hidden state
-    hidden = None
-    music = []
+    count = 0
+    #TODO: Change this back to files not files[]
+    files=files[:1]
+    fileCount = 0
     for f in files:
-        y, sr = lib.load("/home/liam/Desktop/University/2021/MAM3040W/thesis/works/playground/wavs/fma_small/000/" + f, mono=True, duration = duration)
-        data = y
-        data = np.resize(data,((seq_length+1), 1))
-        count = 0
-        for batch_i in (range(0, n_steps)):
+        fileCount+=1
+        hidden = None
+        c0 = None #LSTM!
+        fileData, sr = lib.load("/home/liam/Desktop/University/2021/MAM3040W/thesis/works/playground/wavs/fma_small/000/" + f, mono=True, duration = duration)
+        batch_size = (int)(duration*sr/n_steps)
+        number_of_steps = len(fileData)-batch_size
+        print("BATCH SIZE:",  batch_size, "\nNUMBER OF BATCHES:", n_steps ,"\nNUMBER OF STEPS: ", number_of_steps)
+        for batch_i in (range(0,number_of_steps)):
             # defining the training data
 
             if(train_on_gpu):
                 noizeNet.cuda()
 
-            # time_steps = np.linspace((int)((sr*batch_i)/n_steps), (int)((sr*(batch_i+1))/n_steps), (int)(sr/n_steps))
-            # data = np.resize((seq_length+1), 1)
-            # data[0, sr]
+            time_steps = np.linspace((int)(batch_i), (int)((batch_i + batch_size)), (int)(batch_size))
+            # print(data.size)
+            data = fileData[(batch_i): batch_size + batch_i]
+            # data = np.resize(data, (seq_length+1), 1)
+            data = np.resize(data,((batch_size), 1))
+            # print(data.size, batch_i + batch_size, batch_i)
             # data.resize((seq_length + 1, 1))  # input_size=1
 
             x = data[:-1]
@@ -156,17 +179,20 @@ def train(noizeNet, n_steps, print_every, data, sr, files):
 
             # convert data into Tensors
             x_tensor = torch.Tensor(x).unsqueeze(0)  # unsqueeze gives a 1, batch_size dimension
-            y_tensor = torch.Tensor(y)
+            y_tensor = torch.Tensor(y).unsqueeze(0)
             if(train_on_gpu):
                     x_tensor, y_tensor = x_tensor.cuda(), y_tensor.cuda()
             # print(x_tensor.size())
             # outputs from the rnn
             prediction, hidden = noizeNet(x_tensor, hidden)
 
+            # prediction, hidden, c0 = noizeNet(x_tensor, hidden, c0) #LSTM!
+
             ## Representing Memory ##
             # make a new variable for hidden and detach the hidden state from its history
             # this way, we don't backpropagate through the entire history
             hidden = hidden.data
+            # c0 = c0.data #LSTM!
 
             # calculate the loss
             loss = criterion(prediction, y_tensor)
@@ -176,21 +202,10 @@ def train(noizeNet, n_steps, print_every, data, sr, files):
             loss.backward()
             optimizer.step()
 
-            # display loss and predictions
-            #if batch_i % print_every == 0:
-            count+=1
-            print('Loss: ', loss.item(), "\t num:", count)
-
-            # print(len(x))
-            # plt.plot(time_steps[1:], x[(int)((sr*step)/n_steps), (int)(sr*(step+1)/n_steps)], 'r.')  # input
-            # prediction
-            # music = (prediction.cpu().data.numpy().flatten())
-            # large_time = np.linspace(0, seq_length , seq_length + 1)
-            # plt.plot(time_steps[1:], prediction.cpu().data.numpy().flatten()[(int)((sr*batch_i)/n_steps)+1: (int)(sr*(batch_i+1)/n_steps)], 'g.', markersize=0.2)  # predictions
-            # plt.plot(large_time[1:], prediction.cpu().data.numpy().flatten(), 'g.', markersize=0.2)
-            # plt.show(block = False)
-            
-            # plt.pause(.001)
+            if(int((batch_i/number_of_steps)) % 100 == 0 and batch_i % 100 == 0):
+                print("PROGRESS:\t", round((batch_i/number_of_steps)*100, 2), "%"  , sep="")
+                print("Prediction dimensions:\t", prediction.cpu().data.numpy().flatten()[-5:],"\nY:\t", y[-5:].flatten() ,"\nX:\t" , x[-5:].flatten() ,sep="")
+                print('Loss: ', loss.item(), "\t num:", batch_i, "\t File:", fileCount)
         del prediction
         del hidden
         del x_tensor
@@ -199,17 +214,160 @@ def train(noizeNet, n_steps, print_every, data, sr, files):
         del x
         del y
         gc.collect()
-    # sf.write("./uhh.wav", np.array(music) ,sr)
-    # print(music.type(), np.array(music).type())
-    sf.write('/home/liam/Desktop/University/2021/MAM3040W/thesis/writeup/code/outputSoundFile.wav', music, 22050,format="WAV")
     return noizeNet
 
 ###################################################################################################################
+def predict(noizeNet, duration, n_steps):
+    print("PREDICTING...")
+    noizeNet.eval()
+    hidden = None
+    
+    y, sr = lib.load("/home/liam/Desktop/University/2021/MAM3040W/thesis/works/playground/wavs/fma_small/000/" + files[1], mono=True, duration = duration)
+    data = y
+    # music = np.zeros(shape=y.shape)
+    # data = np.resize(data,((seq_length+1), 1))
+    data = np.random.normal(-1,1,y.shape)
 
-n_steps = 75
-print_every = 5
-test_input = torch.Tensor(data).unsqueeze(0)
-print(test_input.size())
-trained_rnn = train(noizeNet, n_steps, print_every, data = data, sr = sr, files=files)
-torch.save(trained_rnn.state_dict(), "./trained.pt")
+    batch_size = (int)(duration*sr/n_steps)
+    number_of_steps = len(data)-batch_size
+    # music = y[0 : batch_size]
+    music = []
+    next = data[batch_size]
+    fileData = data
+    print("BATCH SIZE:", batch_size ,sep="\t")
+    print("NUMBER OF STEPS:", number_of_steps , sep="\t")
+    # data = data[:batch_size]
+    for batch_i in (range(0, number_of_steps)):
+        
+        # defining the training data
+
+        if(train_on_gpu):
+            noizeNet.cuda()
+
+        # time_steps = np.linspace((int)(batch_i), (int)((batch_i + batch_size)), (int)(batch_size))
+        # print(data.size)
+        data = data[1: batch_size-1]
+        data = np.append(data, next)
+        # print("data",data[-1])
+        # data = np.resize(data, (seq_length+1), 1)
+        data = np.resize(data,((batch_size), 1))
+        # print(data.size, sr*batch_i, step_size*batch_i)
+        # data.resize((seq_length + 1, 1))  # input_size=1
+
+        x = data
+
+        # convert data into Tensors
+        x_tensor = torch.Tensor(x).unsqueeze(0)  # unsqueeze gives a 1, batch_size dimension
+        if(train_on_gpu):
+                x_tensor = x_tensor.cuda()
+
+        prediction, hidden = noizeNet(x_tensor, hidden)
+        
+        music = np.append(music,(prediction.cpu().data.numpy().flatten())[-1])
+        next = prediction.cpu().data.numpy().flatten()[-1]
+        if(int((batch_i/number_of_steps)) % 100 == 0 and batch_i % 100 == 0):
+            print("PROGRESS:\t", round(((batch_i)/number_of_steps)*100, 2), "%"  , sep="")
+            print("Prediction dimensions:\t", prediction.cpu().size(), "\t" ,prediction.cpu().data.numpy().flatten().size, "\nMusic dimensions:\t", music.size ,sep="")
+       
+        hidden = hidden.data
+
+    sf.write('/home/liam/Desktop/University/2021/MAM3040W/thesis/writeup/code/outputSoundFile.wav', music, 22050,format="WAV")
+    time_steps = np.linspace(0, len(music), len(music))
+    plt.plot(time_steps, music,"b.",  markersize=0.1)
+    plt.show()
+    return music
+###################################################################################################################
+
+TRAIN = True
+
+if TRAIN:
+    n_steps = 30
+    print_every = 5
+    step_size =  1
+    print("TRAINING...")
+    trained_rnn = train(noizeNet, n_steps, print_every, data = data, sr = sr, files=files, step_size=step_size)
+    torch.save(trained_rnn.state_dict(), "/home/liam/Desktop/University/2021/MAM3040W/thesis/writeup/smallTrainingV1LSTM.pt")
+    predict(noizeNet=trained_rnn, duration=duration, n_steps=n_steps)
+
+    
+
+else:
+    n_steps = 30
+    input_size=1
+    output_size=1
+    hidden_dim=50
+    step_size =  1
+    n_layers=1
+    # instantiate an RNN
+    noizeNet = NoizeNet(input_size, output_size, hidden_dim, n_layers)
+    # model = TheModelClass(*args, **kwargs)
+    noizeNet.load_state_dict(torch.load("/home/liam/Desktop/University/2021/MAM3040W/thesis/writeup/trainedBtchv2.pt"))
+    predict(noizeNet=noizeNet, duration=duration, n_steps=n_steps)
+    # n_steps = 30
+    # input_size=1
+    # output_size=1
+    # hidden_dim=50
+    # step_size =  1
+    # n_layers=1
+    # # instantiate an RNN
+    # noizeNet = NoizeNet(input_size, output_size, hidden_dim, n_layers)
+    # # model = TheModelClass(*args, **kwargs)
+    # noizeNet.load_state_dict(torch.load("/home/liam/Desktop/University/2021/MAM3040W/thesis/writeup/trainedBtchv2.pt"))
+    # noizeNet.eval()
+    # # prediction, hidden = noizeNet(x_tensor, hidden)
+
+    # hidden = None
+    # music = []
+    # y, sr = lib.load("/home/liam/Desktop/University/2021/MAM3040W/thesis/works/playground/wavs/fma_small/000/" + files[1], mono=True, duration = duration)
+    # data = y
+    # # music = np.zeros(shape=y.shape)
+    # # data = np.resize(data,((seq_length+1), 1))
+    # # data = np.random.normal(-1,1,y.shape)
+
+    # batch_size = (int)(duration*sr/n_steps)
+    # number_of_steps = len(data)-batch_size
+    # music = y[0 : batch_size].flatten()
+    # print(type(music), music.size)
+    # for batch_i in (range(0, number_of_steps)):
+    #     # defining the training data
+
+    #     if(train_on_gpu):
+    #         noizeNet.cuda()
+
+    #     time_steps = np.linspace((int)(batch_i), (int)((batch_i + batch_size)), (int)(batch_size))
+    #     # print(data.size)
+    #     data = data[(batch_i): batch_size + batch_size]
+    #     # data = np.resize(data, (seq_length+1), 1)
+    #     data = np.resize(data,((batch_size), 1))
+    #     # print(data.size, sr*batch_i, step_size*batch_i)
+    #     # print(batch_i, number_of_steps)
+    #     # data.resize((seq_length + 1, 1))  # input_size=1
+
+    #     x = data[:-1]
+    #     y = data[1:]
+
+    #     # convert data into Tensors
+    #     x_tensor = torch.Tensor(x).unsqueeze(0)  # unsqueeze gives a 1, batch_size dimension
+    #     y_tensor = torch.Tensor(y)
+    #     if(train_on_gpu):
+    #             x_tensor, y_tensor = x_tensor.cuda(), y_tensor.cuda()
+    #     # print(x_tensor.size())
+    #     # outputs from the rnn
+    #     prediction, hidden = noizeNet(x_tensor, hidden)
+
+    #     music = np.append(music, (prediction.cpu().data.numpy().flatten()[-1]))
+    #     #music.extend(prediction.cpu().data.numpy().flatten())
+    #     # print((music.size), (prediction.cpu().data.numpy().flatten()[-1]))
+    #     ## Representing Memory ##
+    #     # make a new variable for hidden and detach the hidden state from its history
+    #     # this way, we don't backpropagate through the entire history
+    #     hidden = hidden.data
+    # # print(music[-5:])
+    # time_steps = np.linspace(0, music.size, music.size)
+ 
+    # sf.write('/home/liam/Desktop/University/2021/MAM3040W/thesis/writeup/code/outputSoundFile.wav', music, 22050,format="WAV")
+    # plt.plot(time_steps, music)
+
+
+
 
